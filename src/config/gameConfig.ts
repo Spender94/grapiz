2ea60d1e4ba29e8ed4e87d2effe298f645d63245
Spender94 <166048603+Spender94@users.noreply.github.com>
2024-11-29 12:22:01 +0100

    Delete src directory

--- src/config/gameConfig.ts
+++
@@ -1,8 +0,0 @@
-export const GAME_MODES = {
-  ONLINE: 'online',
-  LOCAL: 'local'
-} as const;
-
-export type GameMode = typeof GAME_MODES[keyof typeof GAME_MODES];
-
-export const DEFAULT_GAME_MODE = GAME_MODES.ONLINE;
